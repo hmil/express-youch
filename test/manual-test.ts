411a60acc51b8beb22f1cb2a60456aad94dadcd2
Hadrien Milano <hadrien.milano@appdynamics.com>
2018-10-03 14:33:11 +0200

    Add documentation and tests for addLink

--- test/manual-test.ts
+++ test/manual-test.ts
@@ -3,5 +3,6 @@ import { app } from './server';
 const PORT = 3000;
 
 app.listen(PORT, () => {
+    // tslint:disable-next-line:no-console
     console.log(`Test server is listening on port ${PORT}`);
 });
